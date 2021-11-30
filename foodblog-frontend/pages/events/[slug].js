27baab86bfd0cb2f51389bf307e2af00d99a3711
trudy2021 <gnamutebi@gmail.com>
2021-11-30 14:34:11 +0300

    commit new changes

--- foodblog-frontend/pages/events/[slug].js
+++ foodblog-frontend/pages/events/[slug].js
@@ -1,12 +1,49 @@
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Link from "next/Link";
+import Image from "next/image";
 import { API_URL } from "../../config/index";
 import Layout from "../../components/Layout";
+import styles from "../../styles/Event.module.css";
 
 export default function EventPage(props) {
-  const titleName = props.eventEdna.name.split(" ");
+  const titleName = props.event.name.split(" ");
+
+  const deleteEvent = () => {
+    console.log("delete event");
+  };
   return (
-    <div>
+    <div className={styles.event}>
+      <div className={styles.controls}>
+        <Link href={`events/edit/${props.event.id}`}>
+          <a className={styles.edit}>
+            <FaPencilAlt /> Edit Event
+          </a>
+        </Link>
+        <a href="#" onClick={deleteEvent} className={styles.delete}>
+          <FaTimes /> Delete Event
+        </a>
+      </div>{" "}
       <Layout title={`${titleName[0]} ${titleName[1]}`}>
-        <h1>{props.eventEdna.name} </h1>
+        <span>
+          {props.event.date} at {props.event.time}
+        </span>
+
+        <h1>{props.event.name} </h1>
+        {props.event.image && (
+          <div className={styles.image}>
+            <Image src={props.event.image} width={960} height={600} />
+          </div>
+        )}
+        <h3>Performers</h3>
+        <p>{props.event.performers}</p>
+        <h3>Description</h3>
+        <p>{props.event.description}</p>
+        <h3>Venue {props.event.venue}</h3>
+        <p>{props.event.address}</p>
+
+        <Link href="/">
+          <a className={styles.back}> Go Back</a>
+        </Link>
       </Layout>
     </div>
   );
@@ -16,6 +53,6 @@ export async function getServerSideProps({ query: { slug } }) {
   const res = await fetch(`${API_URL}/api/events/${slug}`);
   const events = await res.json();
   return {
-    props: { eventEdna: events[0] },
+    props: { event: events[0] },
   };
 }
